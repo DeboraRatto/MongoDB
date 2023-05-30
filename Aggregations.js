9b9594fbbfe10232d1f74d4347f3c7804ccd079b
Débora Ratto <90361400+DeboraRatto@users.noreply.github.com>
2023-05-30 13:26:17 -0300

    Add files via upload

---
+++ Aggregations.js
@@ -0,0 +1,75 @@
+[{
+  "_id": "Alemanha",
+  "Carros": [
+    {
+      "_id": {
+        "$oid": "6475f2408372121da34328ab"
+      },
+      "carro": "Polo",
+      "cor": "Branco",
+      "montadora": "Volkswagen",
+      "Montadoras": {
+        "país": "Alemanha"
+      }
+    }
+  ]
+},
+{
+  "_id": "EUA",
+  "Carros": [
+    {
+      "_id": {
+        "$oid": "6475f2408372121da34328aa"
+      },
+      "carro": "Onix",
+      "cor": "Prata",
+      "montadora": "Chevrolet",
+      "Montadoras": {
+        "país": "EUA"
+      }
+    },
+    {
+      "_id": {
+        "$oid": "6475f2408372121da34328ad"
+      },
+      "carro": "Fiesta",
+      "cor": "Vermelho",
+      "montadora": "Ford",
+      "Montadoras": {
+        "país": "EUA"
+      }
+    }
+  ]
+},
+{
+  "_id": "França",
+  "Carros": [
+    {
+      "_id": {
+        "$oid": "6475f2408372121da34328ac"
+      },
+      "carro": "Sandero",
+      "cor": "Prata",
+      "montadora": "Renault",
+      "Montadoras": {
+        "país": "França"
+      }
+    }
+  ]
+},
+{
+  "_id": "Japão",
+  "Carros": [
+    {
+      "_id": {
+        "$oid": "6475f2408372121da34328ae"
+      },
+      "carro": "City",
+      "cor": "Preto",
+      "montadora": "Honda",
+      "Montadoras": {
+        "país": "Japão"
+      }
+    }
+  ]
+}]
